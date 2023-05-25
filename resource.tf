72daf3094e3eca873541f13632fa8d33e5dd74a4
ganesh20101 <115871700+ganesh20101@users.noreply.github.com>
2023-05-26 01:13:01 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "my_instance" {
-  ami           = "ami-0ef66fb3631e10714"
+  ami           = "ami-0607784b46cbe5816"
   instance_type = "t2.micro"
   tags = {
     Name = "My Instance"
